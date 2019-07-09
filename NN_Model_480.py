7af63c5ace89e6c666abb9df94a9d71662f7e33d
leiiiiii <wangxiaolei0430@gmail.com>
2019-07-09 09:31:26 +0200

    delete useless comment

--- NN_Model_480.py
+++ NN_Model_480.py
@@ -13,19 +13,15 @@ timestamp = "{0:%Y-%m-%d-%H-%M-%S}".format(datetime.now())
 def createNeuralNetworkModel(input_size, output_size, learningRate):
     network = input_data(shape=[None, input_size], name="input")
 
-    # network = fully_connected(network, 10, activation="leakyrelu", weights_init=tf.constant_initializer(value=0.01))
     network = fully_connected(network, 10, activation="relu")
 
     network = fully_connected(network, 10, activation="relu")
 
     network = fully_connected(network, 10, activation="relu")
 
-
-    # network = fully_connected(network, output_size, activation="softmax", weights_init=tf.constant_initializer(value=0.01))
     network = fully_connected(network, output_size, activation="softmax")
 
     network = regression(network, optimizer='sgd', learning_rate=learningRate, loss="categorical_crossentropy",name="targets")
-    #network = regression(network, optimizer="adam", learning_rate=learningRate, loss="categorical_crossentropy", name="targets")
 
     #model = tflearn.DNN(network,tensorboard_dir='log/' + timestamp, tensorboard_verbose=1)
     model = tflearn.DNN(network, tensorboard_verbose=0)
