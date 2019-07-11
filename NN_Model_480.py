319067c3a9294cda0b645672fcb2aef4dd3fcfc9
leiiiiii <wangxiaolei0430@gmail.com>
2019-07-11 13:56:09 +0200

    update

--- NN_Model_480.py
+++ NN_Model_480.py
@@ -1,7 +1,6 @@
 import tflearn
 import tensorflow as tf
 from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.optimizers import SGD
 from tflearn.layers.estimator import regression
 from datetime import datetime
 import numpy as np
@@ -13,19 +12,15 @@ timestamp = "{0:%Y-%m-%d-%H-%M-%S}".format(datetime.now())
 def createNeuralNetworkModel(input_size, output_size, learningRate):
     network = input_data(shape=[None, input_size], name="input")
 
-    #tflearn.init_graph(num_cores=8, gpu_memory_fraction=0.5)
+    tflearn.init_graph(gpu_memory_fraction=0.2)
 
-    network = fully_connected(network, 10, activation="relu")
+    network = fully_connected(network, 12, activation="sigmoid",regularizer='L2', weights_init= tf.constant_initializer(0.03))
 
-    network = fully_connected(network, 10, activation="relu")
+    network = fully_connected(network, output_size, activation="softmax",weights_init= tf.constant_initializer(0.02),regularizer='L2')
 
-    network = fully_connected(network, 10, activation="relu")
+    network = regression(network, optimizer='RMSProp', learning_rate=learningRate, loss="categorical_crossentropy",name="targets")
 
-    network = fully_connected(network, output_size, activation="softmax")
-
-    network = regression(network, optimizer='sgd', learning_rate=learningRate, loss="categorical_crossentropy",name="targets")
-
-    #model = tflearn.DNN(network,tensorboard_dir='log/' + timestamp, tensorboard_verbose=1)
+    #model = tflearn.DNN(network,tensorboard_dir='log/' + timestamp, tensorboard_verbose=3)
     model = tflearn.DNN(network, tensorboard_verbose=0)
 
     return model
